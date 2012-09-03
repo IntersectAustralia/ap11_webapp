b8ad5240107974fdf9c1ad38a74a9157055fa996
charles <charles@intersect.org.au>
2012-09-02 18:51:29 -0700

    ckeditor with models for storing uploaded files (using paperclip as backend)

--- config/application.rb
+++ config/application.rb
@@ -68,5 +68,7 @@ module Ap11
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
