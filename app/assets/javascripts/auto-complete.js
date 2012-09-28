3e9f2465f3fc0c7b939266a6c267ad8ebce20fcd
charles <charles@intersect.org.au>
2012-09-27 18:03:52 -0700

    Minimum of 2 characters needed for FOR codes. Loading all options impacted performance on IE/FF.

--- app/assets/javascripts/auto-complete.js
+++ app/assets/javascripts/auto-complete.js
@@ -1,35 +1,44 @@
 $(function (){
     $("#input_collection_for_code1").select2({
-        placeholder: "Select a code"
+        placeholder: "Select a code",
+        minimumInputLength: 2
     });
     $("#input_collection_for_code2").select2({
         placeholder: "Select a code",
-        allowClear: true
+        allowClear: true,
+        minimumInputLength: 2
     });
     $("#input_collection_for_code3").select2({
         placeholder: "Select a code",
-        allowClear: true
+        allowClear: true,
+        minimumInputLength: 2
     });
     $("#output_collection_for_code1").select2({
-        placeholder: "Select a code"
+        placeholder: "Select a code",
+        minimumInputLength: 2
     });
     $("#output_collection_for_code2").select2({
         placeholder: "Select a code",
-        allowClear: true
+        allowClear: true,
+        minimumInputLength: 2
     });
     $("#output_collection_for_code3").select2({
         placeholder: "Select a code",
-        allowClear: true
+        allowClear: true,
+        minimumInputLength: 2
     });
     $("#user_for_code1").select2({
-        placeholder: "Select a code"
+        placeholder: "Select a code",
+        minimumInputLength: 2
     });
     $("#user_for_code2").select2({
         placeholder: "Select a code",
-        allowClear: true
+        allowClear: true,
+        minimumInputLength: 2
     });
     $("#user_for_code3").select2({
         placeholder: "Select a code",
-        allowClear: true
+        allowClear: true,
+        minimumInputLength: 2
     });
 });
