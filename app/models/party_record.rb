a4d6e1b3021943d4d7b9f68a7687754144543cb0
charles <charles@intersect.org.au>
2012-10-04 16:47:14 -0700

    AP11-93: Party record abbreviations are unique

--- app/models/party_record.rb
+++ app/models/party_record.rb
@@ -2,6 +2,8 @@ class PartyRecord < ActiveRecord::Base
   attr_accessible :abbreviation, :access_rights, :address, :description, :for_code1, :for_code2, :for_code3,
                   :full_name, :license, :url
 
+  before_validation :strip_whitespace
+
   validates_presence_of :abbreviation
   validates_presence_of :full_name
   validates_presence_of :description
@@ -12,4 +14,12 @@ class PartyRecord < ActiveRecord::Base
   validates_length_of :abbreviation, :maximum => 25
   validates_length_of :full_name, :maximum => 255
   validates_length_of :url, :maximum => 2000
+
+  validates_uniqueness_of :abbreviation, :case_sensitive => false
+
+  protected
+
+  def strip_whitespace
+    self.abbreviation.strip! unless abbreviation.blank?
+  end
 end
