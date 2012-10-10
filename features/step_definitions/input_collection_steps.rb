1f4e6bc703b8287dc267375d8107f013873f1c02
charles <charles@intersect.org.au>
2012-10-09 22:30:57 -0700

    AP11-120: Check null before calling party records

--- features/step_definitions/input_collection_steps.rb
+++ features/step_definitions/input_collection_steps.rb
@@ -1,9 +1,7 @@
 When /^I add the FOR code "(.*?)" in input collection$/ do |code|
   find("#s2id_input_collection_for_code1 a.select2-choice").click
   find(".select2-input").set(code)
-  find(".select2-result-label", '').click
-
-  #find(".select2-match", code).click
+  find(".select2-result-label", code).click
 end
 
 Given /^I have the following FOR codes$/ do |table|
