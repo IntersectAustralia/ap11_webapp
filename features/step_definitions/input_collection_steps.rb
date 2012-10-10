a98811ac8fa85d055877d4ab05a609e837ab59db
charles <charles@intersect.org.au>
2012-10-09 23:16:48 -0700

    Input collection step update

--- features/step_definitions/input_collection_steps.rb
+++ features/step_definitions/input_collection_steps.rb
@@ -1,7 +1,6 @@
 When /^I add the FOR code "(.*?)" in input collection$/ do |code|
   find("#s2id_input_collection_for_code1 a.select2-choice").click
   find(".select2-input").set(code)
-  find(".select2-result-label", code).click
 end
 
 Given /^I have the following FOR codes$/ do |table|
