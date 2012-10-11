43e9fa67eead542b11a541f86febdba42c5293bc
charles <charles@intersect.org.au>
2012-10-10 22:52:18 -0700

    FOR-code select fields now use select2 autocomplete in the party records forms

--- app/assets/javascripts/auto-complete.js
+++ app/assets/javascripts/auto-complete.js
@@ -45,4 +45,19 @@ $(function (){
         placeholder: "Select a party record",
         allowClear: true
     })
+    $("#party_record_for_code1").select2({
+        placeholder: "Select a code",
+        allowClear: true,
+        minimumInputLength: 2
+    });
+    $("#party_record_for_code2").select2({
+        placeholder: "Select a code",
+        allowClear: true,
+        minimumInputLength: 2
+    });
+    $("#party_record_for_code3").select2({
+        placeholder: "Select a code",
+        allowClear: true,
+        minimumInputLength: 2
+    });
 });
