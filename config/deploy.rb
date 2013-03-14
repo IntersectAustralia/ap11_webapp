8af5b4f37889b840d9e7dd7e683ffa490b143828
PrzemyslawP <przemyslaw@intersect.org.au>
2013-03-14 15:45:00 +1100

    Changes for local deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :shell, '/bin/bash'
 #set :rvm_ruby_string, 'ruby-1.8.7-p371@ap11'
 set :rvm_type, :user
 set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"") # Read from local system
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+#$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require 'rvm/capistrano'
 set :scm, 'git'
 set :repository, 'git@github.com:IntersectAustralia/ap11_webapp.git'
