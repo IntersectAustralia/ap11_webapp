70d9367f3e0140f895e26f320de5b14fe6398b4d
PrzemyslawP <przemyslaw@intersect.org.au>
2013-06-25 17:01:51 +1000

    Tweaking for deployment

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,13 +1,15 @@
 
 mkdir ~/tmp
 mysql -u root < /home/devel/code_base/ap11_webapp-master/config/deploy.sql
-bundle install
-gem install passenger
-passenger-install-apache2-module < ~/pressEnter
+#bundle install
+gem install capistrano
+gem install capistrano-ext
+#passenger-install-apache2-module < ~/pressEnter
 cap production_local deploy:setup  <~/password
 cap production_local deploy:update <~/password
 cap production_local deploy:schema_load <~/password
 cap production_local deploy:seed <~/password
 cap production_local deploy:populate <~/password
 chmod 777 -R /home/devel/ap11-web
+sudo -S service apache2 restart <~/password
 
