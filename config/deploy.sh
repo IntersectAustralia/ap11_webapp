46abc7f1ee53127b89138eb06ecec2c21d23cca0
Stanley Hon <stanleyhon348@gmail.com>
2013-08-26 15:53:25 +1000

    added gem install passenger to deploy.sh

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,9 +1,9 @@
-
 mkdir ~/tmp
 mysql -u root < /home/devel/code_base/ap11_webapp/config/deploy.sql
 #bundle install
 gem install capistrano
 gem install capistrano-ext
+gem install passenger -v4.0.14
 #passenger-install-apache2-module < ~/pressEnter
 cap production_local deploy:setup  <~/password
 cap production_local deploy:update <~/password
